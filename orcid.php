910bf4b99265f7425b740f23de019b0a4618c383
Amaresh R Joshi <joshia@msu.edu>
2020-04-24 21:54:25 -0400

    Testing commit from PHPStorm (commit #2)

--- orcid.php
+++ orcid.php
@@ -15,10 +15,10 @@ add_action( 'the_content', 'display_orcid_info' );
 
 function display_orcid_info ( $content ) {
     // return $content .= '<p>Thank you for reading!</p>';
-    // $orcidID = "0000-0003-0265-9119"; // Alan Munn
+    $orcidID = "0000-0003-0265-9119"; // Alan Munn
     //$orcidID = "0000-0003-1822-3109";  // Bronson Hui
     //$orcidID = "0000-0003-3953-7940"; // Chris Long (U of CO at Boulder)
-    $orcidID = "0000-0002-5251-0307"; // Kathleen Fitzpatrick
+    //$orcidID = "0000-0002-5251-0307"; // Kathleen Fitzpatrick
     //$orcidID = "0000-0002-8143-2408"; // Scott Schopieray
 
     $orcid_data = get_orcid_data($orcidID);
